5d2261ac845e97a13d33aa385fed147c57dd414b
sarojthapa <sarojthapa199@gmail.com>
2024-10-04 19:55:14 +0545

    Added metadata

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,18 @@ import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
 import SectionHeaders from "@/components/layout/SectionHeaders";
 
+export const metadata = {
+   title: "Food Ordering App",
+   description:
+      "Order your favorite meals effortlessly with our food ordering app. Browse restaurants, explore menus, and enjoy quick delivery or convenient pickup",
+};
+
 export default function Home() {
    return (
       <>
          <Hero />
          <HomeMenu />
-         <section className='text-center my-16' id="about">
+         <section className='text-center my-16' id='about'>
             <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
             <div className='text-gray-500 max-w-md mx-auto mt-8 flex flex-col gap-4'>
                <p>
@@ -28,7 +34,7 @@ export default function Home() {
                </p>
             </div>
          </section>
-         <section className='text-center my-8' id="contact">
+         <section className='text-center my-8' id='contact'>
             <SectionHeaders
                subHeader={"Don't hesitate"}
                mainHeader={"Contact us"}
